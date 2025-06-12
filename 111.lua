0acd2933d1799face79a4b1d96549ec050b7dc2d
rie20s <testflex123123@gmail.com>
2025-06-12 10:12:33 +0300

    Update 111.lua

--- 111.lua
+++ 111.lua
@@ -1,4 +1,4 @@
 if getgenv().AutoExecute == true then
     Auto_Execute = true
 end
-loadstring(game:HttpGet('https://hungquan99.xyz/Script/HungHub.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/rie20s/ade/refs/heads/main/222.lua'))()
